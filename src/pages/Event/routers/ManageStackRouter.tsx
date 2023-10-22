1c20fead98afc607c6ac8dd9079f574a7477207a
Kanlayakorn Yeenang <78673905+KanlayakornYeenang@users.noreply.github.com>
2023-10-22 18:21:56 +0700

    Update ManageStackRouter.tsx

--- src/pages/Event/routers/ManageStackRouter.tsx
+++ src/pages/Event/routers/ManageStackRouter.tsx
@@ -4,70 +4,90 @@ import { StackNavigationProp } from "@react-navigation/stack"
 //pages
 import WrappedEditStaff from "../pages/manage/EditStaff";
 import ManageEvent from "./tabs/ManageEvent";
-import { NavigationContainer} from '@react-navigation/native';
-import {  RouteProp } from '@react-navigation/native';
-type PictureType ={
-    attributes:{
-      url:string
-    }
-  }
-type staffType = {
-    attributes:{
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import { RouteProp } from '@react-navigation/native';
+import WrappedEditEvent from "../../Events/pages/EditEvent";
+type PictureType = {
+    attributes: {
         url: string
-    }| undefined,
-}
-export type ManageStackRouterType  = {
-    ManageEvent: {
-        eventId:number, 
-        eventName:string,
-        eventDescription:string,
-        eventPicture:Object|undefined,
-        eventStart:string,
-        eventEnd:string,
-        eventOwnerId:string,
     }
-    EditStaff:{
-        eventName: string |undefined,
-        eventDescription: string |undefined,
+}
+type staffType = {
+    attributes: {
+        url: string
+    } | undefined,
+}
+export type ManageStackRouterType = {
+    ManageEvent: {
+        eventId: number,
+        eventName: string,
+        eventDescription: string,
+        eventPicture: Object | undefined,
+        eventStart: string,
+        eventEnd: string,
+        eventOwnerId: string,
+    }
+    EditStaff: {
+        eventName: string | undefined,
+        eventDescription: string | undefined,
         eventPicture: PictureType | undefined,
         staffDuty: string | undefined,
         staffPosition: string | undefined,
-        staffPicture: staffType |undefined
+        staffPicture: staffType | undefined
         staffUsername: string | undefined,
-        staffStudentId:string | undefined,
-        staffId:string | undefined
+        staffStudentId: string | undefined,
+        staffId: string | undefined
     },
+    EditEvent: {
+        eventId: number | undefined
+    }
 }
 const Stack = createStackNavigator<ManageStackRouterType>()
 
 type ManageStackRouterProp = {
     route: RouteProp<ManageStackRouterType, 'ManageEvent'>
-    navigation : StackNavigationProp<ManageStackRouterType, 'ManageEvent'>
-    eventId : number ,
-    eventName:string
-    eventDescription:string
-    eventPicture:Object|undefined
-    eventStart:string,
-    eventEnd:string
+    navigation: StackNavigationProp<ManageStackRouterType, 'ManageEvent'>
+    eventId: number,
+    eventName: string
+    eventDescription: string
+    eventPicture: Object | undefined
+    eventStart: string,
+    eventEnd: string
 }
 
-const ManageStackRouter = ({route, navigation, eventId, eventName, eventDescription, eventEnd,eventPicture,eventStart}: ManageStackRouterProp)  =>{
-    return(
-            <Stack.Navigator
-            
-                initialRouteName="ManageEvent"
-                screenOptions={{
-                    headerStyle: { backgroundColor: "white" },
-                    headerTitleStyle: { fontFamily: "noto", color: process.env.EXPO_PUBLIC_PRIMARY_COLOR },
-                }}
-            >
-                <Stack.Screen name="ManageEvent"  options={{ headerShown: false }} initialParams={{eventId:eventId, eventName:eventName, eventDescription:eventDescription, eventPicture:eventPicture, eventStart:eventStart, eventEnd:eventEnd}}>
-                    {(props)=><ManageEvent  route={props.route} navigation={props.navigation}/>}
-                </Stack.Screen> 
-                <Stack.Screen name="EditStaff"  options={{headerShown:false}}>
-                    {(props)=><WrappedEditStaff route={props.route} navigation={props.navigation}/>}
-                </Stack.Screen>
-            </Stack.Navigator>
+const ManageStackRouter = ({ route, navigation, eventId, eventName, eventDescription, eventEnd, eventPicture, eventStart }: ManageStackRouterProp) => {
+    const routeName = getFocusedRouteNameFromRoute(route)
+    React.useLayoutEffect(() => {
+        if (routeName === "EditEvent") {
+            // @ts-ignore
+            navigation.setOptions({ tabBarStyle: { display: 'none' } })
+            navigation.getParent()?.setOptions({ headerShown: false })
+        } else {
+            // @ts-ignore
+            navigation.setOptions({ tabBarStyle: { display: 'flex' } })
+            navigation.getParent()?.setOptions({ headerShown: true })
+        }
+    })
+    return (
+        <Stack.Navigator
+            initialRouteName="ManageEvent"
+            screenOptions={{
+                headerStyle: { backgroundColor: "white" },
+                headerTitleStyle: { fontFamily: "noto-semibold", color: "#000" },
+                headerTitleAlign: "center"
+            }}
+        >
+            <Stack.Screen name="ManageEvent" options={{ headerShown: false }} initialParams={{ eventId: eventId, eventName: eventName, eventDescription: eventDescription, eventPicture: eventPicture, eventStart: eventStart, eventEnd: eventEnd }}>
+                {(props) => <ManageEvent route={props.route} navigation={props.navigation} />}
+            </Stack.Screen>
+            <Stack.Screen name="EditStaff" options={{ headerShown: false }}>
+                {(props) => <WrappedEditStaff route={props.route} navigation={props.navigation} />}
+            </Stack.Screen>
+            {/* @ts-ignore */}
+            <Stack.Screen name="EditEvent" options={{ title: "แก้ไขกิจกรรม" }}>
+                {(props) => <WrappedEditEvent route={props.route} navigation={props.navigation} />}
+            </Stack.Screen>
+        </Stack.Navigator>
     )
 
 }
